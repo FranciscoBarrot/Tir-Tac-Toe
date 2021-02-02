4f64a12d707228fff6cd053952d383471534d9ee
Barrot Francisco <franciscobarrot98@gmail.com>
2021-02-02 12:06:37 -0300

    gameboard styles added

---
+++ main.js
@@ -0,0 +1,13 @@
+const gameBoard = (() => {
+  const board = Array(9)
+  return { board }
+})()
+
+const displayController = ((doc) => {
+  const display = (array) => {
+    array.forEach((element) => {})
+  }
+  return { display }
+})(document)
+
+const players = () => {}
