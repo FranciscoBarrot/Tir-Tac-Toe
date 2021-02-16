264cc18fef69d4be2a8fbf6a83e25bc71ae34783
Barrot Francisco <franciscobarrot98@gmail.com>
2021-02-16 12:45:26 -0300

    it works!!

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const gameBoard = (() => {
   const board = Array(9)
-  return { board }
+  const markTheSpot = (index, mark) => {
+    if (board[index] === undefined) {
+      board[index] = mark
+    }
+  }
+  return { board, markTheSpot }
 })()
 
 const displayController = ((doc) => {
@@ -12,10 +17,27 @@ const displayController = ((doc) => {
   return { display }
 })(document)
 
-displayController.display(gameBoard.board)
-
-const players = () => {
-  const score
+const players = (mark) => {
+  return { mark }
 }
 
-const game = (() => {})()
+const game = ((doc) => {
+  let turn = 1
+  const play = (e) => {
+    if (turn === 1) {
+      gameBoard.markTheSpot(e.target.dataset.index, player1.mark)
+      turn = 2
+    } else {
+      turn = 1
+      gameBoard.markTheSpot(e.target.dataset.index, player2.mark)
+    }
+    displayController.display(gameBoard.board)
+  }
+  doc.querySelectorAll('.square').forEach((square) => {
+    square.addEventListener('click', play)
+  })
+})(document)
+
+/* displayController.display(gameBoard.board) */
+const player1 = players('x')
+const player2 = players('o')
